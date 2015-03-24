f7e911c72fed4c6244b171d99f5894a910b64f99
Dave Compton <davecompton7@gmail.com>
2015-03-24 09:46:35 -0700

    now using threshold instead of fixed number of terms

--- Main.hs
+++ Main.hs
@@ -1,15 +1,31 @@
+import System.Environment
 import Data.Ratio
 
--- compute atan x using n terms of taylor series
-atanTaylor x n = sum $ take n $ zipWith (/) (iterate (*(x*x*(-1))) x) $ [1,3..]
+-- compute atan x using terms of taylor series greater than threshold
+atanTaylor :: Rational -> Rational -> Rational
+atanTaylor x threshold = sum terms where
+    terms = takeWhile ((threshold <).abs) $ zipWith (/) (iterate (*negXSqrd) x) [1,3..] where
+        negXSqrd = -x*x -- improves readabilty but had no effect on performance
 
--- pi/4 = 4 * atan 1/5 - atan 1/239
-pi' = 4*(4 * atanTaylor (1%5) 2000 - atanTaylor (1%239) 2000)
+-- (exact formula): pi/4 = 4 * atan 1/5 - atan 1/239
+pi' :: Int -> Rational
+pi' digits = 4*(4 * atanTaylor (1%5) threshold - atanTaylor (1%239) threshold) where
+    threshold = 1 % 10^digits
 
 -- compute digits by adding zeros to numerator and dividing
-pi_digits = 10^(length $ show $ denominator pi') * numerator pi' `div` denominator pi'
+piDigits :: Int -> String
+piDigits digits = show $ 10^length (show piDen) * piNum `div` piDen where
+    computedPi = pi' digits
+    piDen = denominator computedPi
+    piNum = numerator computedPi
 
+-- compute pi to number of digits requested on cmd line
+-- then count correct digits by comparing to precomputed digits
+main :: IO ()
 main = do
-        checkDigits <- getContents
-        print $ length $ takeWhile (uncurry (==)) $ zip (show pi_digits) checkDigits
+    args <- getArgs
+    let digitsToCompute = read $ head args::Int
+    let computedDigits = piDigits digitsToCompute
+    checkDigits <- getContents
+    print $ length $ takeWhile (uncurry (==)) $ zip computedDigits checkDigits
 
