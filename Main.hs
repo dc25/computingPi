2fd6b5790c5e9c6e2b3430b9b669ef9068d8a3eb
Dave Compton <davecompton7@gmail.com>
2015-03-24 10:04:09 -0700

    changed some comments

--- Main.hs
+++ Main.hs
@@ -7,7 +7,8 @@ atanTaylor x threshold = sum terms where
     terms = takeWhile ((threshold <).abs) $ zipWith (/) (iterate (*negXSqrd) x) [1,3..] where
         negXSqrd = -x*x -- improves readabilty but had no effect on performance
 
--- (exact formula): pi/4 = 4 * atan 1/5 - atan 1/239
+-- (exact formula thanks to wikipedia): pi/4 = 4 * atan 1/5 - atan 1/239
+-- http://en.wikipedia.org/wiki/Machin-like_formula
 pi' :: Int -> Rational
 pi' digits = 4*(4 * atanTaylor (1%5) threshold - atanTaylor (1%239) threshold) where
     threshold = 1 % 10^digits
