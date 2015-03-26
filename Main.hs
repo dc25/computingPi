3b5bd6ad9bf051a9f087a93567a89fc790364c1e
Dave Compton <davecompton7@gmail.com>
2015-03-25 18:29:13 -0700

    removed unnecessary type specification

--- Main.hs
+++ Main.hs
@@ -1,17 +1,20 @@
 import System.Environment
 import Data.Ratio
 
+-- generate all the terms for the Taylor series representation of atan x
 atanTaylorTerms :: Rational -> [Rational]
-atanTaylorTerms x = zipWith (/) (iterate ((-x*x)*) x) [1,3..] -- from wikipedia
+atanTaylorTerms x = zipWith (/) (iterate ((-x*x)*) x) [1,3..] 
 
+-- Take the terms up to a limit of the Taylor series of 
+-- atan x, add them up to get atan x. 
 atanTaylor :: Rational -> Rational -> Rational
 atanTaylor x termLimit = sum $ takeWhile ((termLimit <).abs) $ atanTaylorTerms x
 
 -- (exact formula thanks to wikipedia): pi/4 = 4 * atan 1/5 - atan 1/239
 -- http://en.wikipedia.org/wiki/Machin-like_formula
 pi' :: Int -> Rational
-pi' digits = 4*sum [scale * atanTaylor arg termLimit | (scale, arg) <- parameters, let termLimit = (1%10^digits)/abs scale] where 
-    parameters = [ ( 183, 1%239), (  32, 1%1023), ( -68, 1%5832), (  12, 1%110443), ( -12, 1%4841182), (-100, 1%6826318) ]
+pi' digits = 4*sum [scale * atanTaylor x termLimit | (scale, x) <- parameters, let termLimit = (1%10^digits)/abs scale] where 
+    parameters = [ (183, 1%239), (32, 1%1023), (-68, 1%5832), (12, 1%110443), (-12, 1%4841182), (-100, 1%6826318) ]
 
 -- compute digits by adding zeros to numerator and dividing
 piDigits :: Int -> String
@@ -25,7 +28,7 @@ piDigits digits = show $ 10^length (show piDen) * piNum `div` piDen where
 main :: IO ()
 main = do
     args <- getArgs
-    let digitsToCompute = read $ head args::Int
+    let digitsToCompute = read $ head args
     let computedDigits = piDigits digitsToCompute
     checkDigits <- getContents
     print $ length $ takeWhile (uncurry (==)) $ zip computedDigits checkDigits
